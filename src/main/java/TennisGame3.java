9611c2ff28eb1c00eb9d3db988d596497a7c6224
arimohy <arimohy@gmail.com>
2021-09-25 22:53:16 -0500

    refactorizacion archivo 3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,35 +1,64 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int scorePlayer2;
+    private int scorePlayer1;
+    private final String playerName1;
+    private final String playerName2;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String playerName1, String playerName2) {
+        this.playerName1 = playerName1;
+        this.playerName2 = playerName2;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-        }
-    }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        String score;
+        boolean isPlayerScoreEqual=(this.scorePlayer1 == this.scorePlayer2);
+        if(isPlayerScoreEqual && scorePlayer1>=3)
+            score= "Deuce";
         else
-            this.p2 += 1;
+            score= getScoreNotTie(isPlayerScoreEqual);
+        return  score;
+
+    }
+
+    private String getScoreNotTie(boolean isPlayerScoreEqual) {
+        String score= getScoreMore4(isPlayerScoreEqual);
+        if (scorePlayer1 < 4 && scorePlayer2 < 4) {
+            score = getScoreLess4(isPlayerScoreEqual);
+        }
+        return score;
+    }
+
+    private String getScoreMore4(boolean isPlayerScoreEqual) {
+        String score;
+        String player = getPlayerWithMoreScore();
+        int Player1lessPlayer2=Math.abs(scorePlayer1-scorePlayer2);
+        score= getAdvantageorWin(player,Player1lessPlayer2);
+        return score;
+    }
+
+    private String getAdvantageorWin(String player,int Player1lessPlayer2) {
+        return ( Player1lessPlayer2== 1) ? "Advantage " + player : "Win for " + player;
+    }
+
+    private String getPlayerWithMoreScore() {
+        return this.scorePlayer1 > this.scorePlayer2 ? this.playerName1 : this.playerName2;
+    }
+
+    private String getScoreLess4(boolean isPlayerScoreEqual) {
+        String score;
+        String[] listScore = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        score = listScore[scorePlayer1];
+        score+= isPlayerScoreEqual ? "-All" :"-" + listScore[scorePlayer2];
+        return score;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals(playerName1))
+            this.scorePlayer1 += 1;
+        else
+            this.scorePlayer2 += 1;
         
     }
 
